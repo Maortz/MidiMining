03bb7163f593bedbf85132dc9801cb699ae01de2
Maortz <maortz@gmail.com>
2017-02-14 23:31:05 +0200

    note impl

--- MidiMining/Note.cpp
+++ MidiMining/Note.cpp
@@ -10,7 +10,39 @@ Note::~Note()
 {
 }
 
-int Note::getInterval(Note note1, Note note2)
+int Note::getInterval(Note &note1, Note &note2)
 {
 	return note1.m_semitones - note2.m_semitones;
+}
+
+double Note::endBarPosition()
+{
+	return m_bar_position + m_duration;
+}
+
+double Note::endStartPosition()
+{
+	return m_start_position + m_duration;
+}
+
+double Note::ppqnToDuration(unsigned int division, unsigned int ppqn)
+{
+	double whole = division * 4;
+	return (ppqn / whole);
+}
+
+int Note::octaveFromMiddle()
+{
+	return (m_semitones / 12) - 5;
+}
+
+void Note::noteSign(char* buff)
+{
+	int note = m_semitones % 12;
+	char c = 'C';
+	if (note <= 4)
+		++note;
+	buff[0] = c + note / 2;
+	buff[1] = note % 2 == 0 ? '\0' : '#';
+	buff[2] = '\0';
 }
--- MidiMining/Note.h
+++ MidiMining/Note.h
@@ -4,7 +4,7 @@ class Note
 public:
 
 	unsigned int m_semitones;
-	unsigned int m_duration;
+	double m_duration;
 	double m_bar_position;
 	double m_start_position;
 
@@ -12,9 +12,14 @@ public:
 	~Note();
 
 	double endBarPosition();
-	double endstartPosition();
-	
-	static int getInterval(Note note1, Note note2);
+	double endStartPosition();
+	/*
+	Gets: Allocated with 3 chars (last for \0, "A#\0")
+	*/
+	void noteSign(char* buff);
+	int octaveFromMiddle();
 
+	static int getInterval(Note &note1, Note &note2);
+	static double ppqnToDuration(unsigned int division, unsigned int ppqn);
 };
 
