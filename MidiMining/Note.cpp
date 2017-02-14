8a0453e3134f3371075dc9f0c5856c8ae6bced19
Maortz <maortz@gmail.com>
2017-02-14 22:37:00 +0200

    finish some func

--- MidiMining/Note.cpp
+++ MidiMining/Note.cpp
@@ -1,11 +1,36 @@
 #include "Note.h"
 
 
-Note::Note()
+Note::Note() :m_semitones(0), m_duration(0)
 {
 }
 
 
 Note::~Note()
 {
+}
+
+void Note::set_semitones(unsigned int semitones)
+{
+	m_semitones = semitones;
+}
+
+unsigned int Note::get_semitones()
+{
+	return m_semitones;
+}
+
+void Note::set_duration(unsigned int duration)
+{
+	m_duration = duration;
+}
+
+unsigned int Note::get_duration()
+{
+	return m_duration;
+}
+
+int Note::GetInterval(Note note1, Note note2)
+{
+	return note1.m_semitones - note2.m_semitones;
 }
--- MidiMining/Note.h
+++ MidiMining/Note.h
@@ -1,10 +1,17 @@
 #pragma once
 class Note
 {
-	int semitones;
-
+	unsigned int m_semitones;
+	unsigned int m_duration;
 public:
 	Note();
 	~Note();
+
+	void set_semitones(unsigned int note);
+	unsigned int get_semitones();
+	void set_duration(unsigned int note);
+	unsigned int get_duration();
+
+	static int GetInterval(Note note1, Note note2);
 };
 
