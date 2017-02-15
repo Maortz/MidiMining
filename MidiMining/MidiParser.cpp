bcacc47e1bdb42a5e22d6e1cf7a4884e60fb716e
Maortz <maortz@gmail.com>
2017-02-15 22:14:40 +0200

    prototypes of midi parseer

--- MidiMining/MidiParser.cpp
+++ MidiMining/MidiParser.cpp
@@ -1,7 +1,7 @@
 #include "MidiParser.h"
 #include <string.h>
 
-MidiParser::MidiParser(char* midi, int len)
+MidiParser::MidiParser(char* midi, int len) : m_track_count(0), m_events_count(0), m_is_valid(false)
 {
 	m_midi = new char[len];
 	memcpy(m_midi, midi, len);
@@ -10,4 +10,25 @@ MidiParser::MidiParser(char* midi, int len)
 
 MidiParser::~MidiParser()
 {
+}
+
+bool MidiParser::parseIt()
+{
+	m_is_valid = true;
+	return true;
+}
+
+short MidiParser::getChannels()
+{
+
+}
+
+bool MidiParser::getNotes(NoteSeries* notes, int channel)
+{
+
+}
+
+bool MidiParser::isValid()
+{
+	return m_is_valid;
 }
--- MidiMining/MidiParser.h
+++ MidiMining/MidiParser.h
@@ -1,4 +1,6 @@
 #pragma once
+class NoteSeries;
+
 class MidiParser
 {
 	char * m_midi;
@@ -26,9 +28,40 @@ class MidiParser
 		short number_of_tracks;
 		short division;
 	};
-
+	HeaderChunk* m_header_chunk;
+	TrackChunk* m_track_chunk;
+	unsigned int m_track_count;
+	TrackEvent* m_track_event;
+	unsigned int **m_events_count;
+	bool m_is_valid;
 public:
+
+	const char ShortBits[] = {
+		0x0001,
+		0x0002,
+		0x0004,
+		0x0008,
+		0x0010,
+		0x0020,
+		0x0040,
+		0x0080,
+		0x0100,
+		0x0200,
+		0x0400,
+		0x0800,
+		0x1000,
+		0x2000,
+		0x4000,
+		0x8000
+	};
+
 	MidiParser(char* midi, int len);
 	~MidiParser();
+
+	bool isValid();
+	bool parseIt();
+	short getChannels();
+	bool getNotes(NoteSeries* notes, int channel);
+
 };
 
