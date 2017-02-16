7323a7a769847f62baa2e055f50c8bbce8303a9f
Maortz <maortz@gmail.com>
2017-02-16 20:18:57 +0200

    Add program instruments

--- MidiMining/MidiParser.h
+++ MidiMining/MidiParser.h
@@ -1,4 +1,5 @@
 #pragma once
+#include "Note.h"
 class NoteSeries;
 
 class MidiParser
@@ -20,6 +21,234 @@ class MidiParser
 		char* data;
 	};
 
+	class EventI
+	{};
+	class EventI2
+	{};
+
+	class NoteEvent : public EventI2
+	{
+	public:
+		NoteEvent();
+		~NoteEvent();
+
+		Note m_note;
+	private:
+
+	};
+
+	enum InstrumentFamiles
+	{
+		Piano = 1,
+		Chromatic_Percussion = 9,
+		Organ = 17,
+		Guitar = 25,
+		Bass = 33,
+		Strings = 41,
+		Ensemble = 49,
+		Brass = 57,
+		Reed = 65,
+		Pipe = 73,
+		Synth_Lead = 81,
+		Synth_Pad = 89,
+		Synth_Effects = 97,
+		Ethnic = 105,
+		Percussive = 113,
+		Sound_Effects = 121
+	};
+
+	enum Instruments
+	{
+		Acoustic_Grand_Piano = 1,
+		Bright_Acoustic_Piano,
+		Electric_Grand_Piano,
+		Honky_tonk_Piano,
+		Electric_Piano_1_Rhodes_Piano,
+		Electric_Piano_2_Chorused_Piano,
+		Harpsichord,
+		Clavinet,
+		Celesta,
+		Glockenspiel,
+		Music_Box,
+		Vibraphone,
+		Marimba,
+		Xylophone,
+		Tubular_Bells,
+		Dulcimer_Santur,
+		Drawbar_Organ_Hammond,
+		Percussive_Organ,
+		Rock_Organ,
+		Church_Organ,
+		Reed_Organ,
+		Accordion_French,
+		Harmonica,
+		Tango_Accordion_Band_neon,
+		Acoustic_Guitar_nylon,
+		Acoustic_Guitar_steel,
+		Electric_Guitar_jazz,
+		Electric_Guitar_clean,
+		Electric_Guitar_muted,
+		Overdriven_Guitar,
+		Distortion_Guitar,
+		Guitar_harmonics,
+		Acoustic_Bass,
+		Electric_Bass_fingered,
+		Electric_Bass_picked,
+		Fretless_Bass,
+		Slap_Bass_1,
+		Slap_Bass_2,
+		Synth_Bass_1,
+		Synth_Bass_2,
+		Violin,
+		Viola,
+		Cello,
+		Contrabass,
+		Tremolo_Strings,
+		Pizzicato_Strings,
+		Orchestral_Harp,
+		Timpani,
+		String_Ensemble_1_strings,
+		String_Ensemble_2_slow_strings,
+		SynthStrings_1,
+		SynthStrings_2,
+		Choir_Aahs,
+		Voice_Oohs,
+		Synth_Voice,
+		Orchestra_Hit,
+		Trumpet,
+		Trombone,
+		Tuba,
+		Muted_Trumpet,
+		French_Horn,
+		Brass_Section,
+		SynthBrass_1,
+		SynthBrass_2,
+		Soprano_Sax,
+		Alto_Sax,
+		Tenor_Sax,
+		Baritone_Sax,
+		Oboe,
+		English_Horn,
+		Bassoon,
+		Clarinet,
+		Piccolo,
+		Flute,
+		Recorder,
+		Pan_Flute,
+		Blown_Bottle,
+		Shakuhachi,
+		Whistle,
+		Ocarina,
+		Lead_1_square_wave,
+		Lead_2_sawtooth_wave,
+		Lead_3_calliope,
+		Lead_4_chiffer,
+		Lead_5_charang,
+		Lead_6_voice_solo,
+		Lead_7_fifths,
+		Lead_8_bass_lead,
+		Pad_1_new_age_Fantasia,
+		Pad_2_warm,
+		Pad_3_polysynth,
+		Pad_4_choir_space_voice,
+		Pad_5_bowed_glass,
+		Pad_6_metallic_pro,
+		Pad_7_halo,
+		Pad_8_sweep,
+		FX_1_rain,
+		FX_2_soundtrack,
+		FX_3_crystal,
+		FX_4_atmosphere,
+		FX_5_brightness,
+		FX_6_goblins,
+		FX_7_echoes_drops,
+		FX_8_sci_fi_star_theme,
+		Sitar,
+		Banjo,
+		Shamisen,
+		Koto,
+		Kalimba,
+		Bag_pipe,
+		Fiddle,
+		Shanai,
+		Tinkle_Bell,
+		Agogo,
+		Steel_Drums,
+		Woodblock,
+		Taiko_Drum,
+		Melodic_Tom,
+		Synth_Drum,
+		Reverse_Cymbal,
+		Guitar_Fret_Noise,
+		Breath_Noise,
+		Seashore,
+		Bird_Tweet,
+		Telephone_Ring,
+		Helicopter,
+		Applause,
+		Gunshot
+	};
+
+	class ProgramEvent : public EventI2
+	{
+	public:
+		ProgramEvent();
+		~ProgramEvent();
+
+
+	private:
+
+	};
+
+	class SymbolsEvent : public EventI2
+	{
+	public:
+		SymbolsEvent();
+		~SymbolsEvent();
+
+	private:
+
+	};
+
+	class MidiEvent : public EventI
+	{
+	public:
+		MidiEvent();
+		~MidiEvent();
+
+		char m_channel;
+		EventI2* m_details;
+	private:
+
+	};
+
+	class MetaEvent : public EventI
+	{
+	public:
+		MetaEvent();
+		~MetaEvent();
+
+	private:
+
+	};
+
+	class SysexEvent : public EventI
+	{
+	public:
+		SysexEvent();
+		~SysexEvent();
+
+	private:
+
+	};
+
+	enum EventType
+	{
+		MidiEvent,
+		MetaEvent,
+		SysexEvent
+	};
+
 	class Event
 	{
 	public:
@@ -28,20 +257,20 @@ class MidiParser
 		Event(char* event_buff);
 		~Event();
 		bool parse();
-		inline int get_delta_time() { return m_delta_time; }
-		inline char get_status() { return m_status; }
+		void set_delta_time();
+		void set_status();
 		inline int get_buff_len() { return m_buff_len; }
 
-		char* m_data;
 		bool m_is_valid;
-	private:
-		int m_buff_len;
-
 		unsigned int m_delta_time;
 		// midi_event:  status, 0x80 - 0xEF
 		// meta_event:  0xFF
 		// sysex_event: 0xF0
 		char m_status;
+		EventI* m_event_details;
+		EventType type;
+	private:
+		int m_buff_len;
 	};
 
 	class Track
